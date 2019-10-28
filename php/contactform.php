eb03fcdb2603b5443cba5a9e86858d45bbcd89d0
miguel_pereira <miguelglp90@gmail.com>
2019-10-28 12:58:20 +0000

    Updates

---
+++ php/contactform.php
@@ -0,0 +1,26 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $message = $_POST['message']; 
+
+
+    $mailTo = "partidodorelogio@hotmail.com";
+    $headers = "Mail de: ".$email;
+    $txt = "Recebeste um email de ".$name. ".\n\n".$message;
+
+    if (empty($name) || empty($message) || empty($email)) {
+        header('Location:../contact.html?signup=empty');
+    } else 
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header("Location:../index.php?signup=invalidEmail&name=$name&username=$username");
+
+        } else {
+            mail($mailTo, $contact, $txt, $headers);
+
+    header("Location: ../contact.html?mailsent");
+        }
+}
